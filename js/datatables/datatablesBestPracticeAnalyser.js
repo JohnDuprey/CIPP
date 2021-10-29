45d0236b0dd17649ae57d27bad9914c6059b867a
John Duprey <jwd@johnwduprey.com>
2021-10-29 07:08:48 -0400

    Squashed commit of the following:
    commit 539a6e8b2cfc12e48b600108225e062abb9298bf
    Merge: ded25b8 cf52a67
    Author: KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
    Date:   Thu Oct 28 22:45:27 2021 +0200
    
        Merge pull request #153 from KelvinTegelaar/dev
    
        Merging dev into master
    
    commit cf52a6747be4c4a11a368c140fdf7a3fd784401c
    Merge: a7ace6c ded25b8
    Author: KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
    Date:   Thu Oct 28 22:44:40 2021 +0200
    
        Merge branch 'master' into dev
    
    commit a7ace6cfae2cb92eb73385c08e7370957099b93a
    Merge: 6208f3e 5c414df
    Author: Gavin Stone <gavsto@gmail.com>
    Date:   Thu Oct 28 20:29:51 2021 +0000
    
        Merge pull request #152 from gavsto/dev
    
        ENHANCEMENT: Added Scroll X for datatable
    
    commit 5c414df3be9653693fac5c2eec4417cd9f1ecea3
    Author: Gavsto <gavsto@gmail.com>
    Date:   Thu Oct 28 20:28:55 2021 +0000
    
        ENHANCEMENT: Added Scroll X for datatable
    
    commit 6208f3ef59cd92834b5a49817caf3ac191d46cdb
    Author: Kelvin Tegelaar <kelvin@limenetworks.nl>
    Date:   Thu Oct 28 22:23:09 2021 +0200
    
        lets just push
    
    commit ebc8a3aac5b3de985866c4ae59ca7f2fe7fda57e
    Author: Kelvin Tegelaar <kelvin@limenetworks.nl>
    Date:   Thu Oct 28 20:32:44 2021 +0200
    
        changed timezon stuff.
    
    commit a081b1af44f7ac1975d2f2e8a8dd48d3916ac3e8
    Merge: 7177269 e1d38e8
    Author: Gavin Stone <gavsto@gmail.com>
    Date:   Thu Oct 28 16:45:02 2021 +0000
    
        Merge pull request #149 from gavsto/dev
    
        DOCUMENTATION: Update a few documents
    
    commit e1d38e8cc03e957fa8cb9c272208ea5180709c38
    Merge: aaed325 7177269
    Author: Gavsto <gavsto@gmail.com>
    Date:   Thu Oct 28 16:42:17 2021 +0000
    
        Merge remote-tracking branch 'origin/dev' into dev
    
    commit aaed3257e2224953020a672a89b9f84ca3ee6ef3
    Author: Gavsto <gavsto@gmail.com>
    Date:   Thu Oct 28 16:41:58 2021 +0000
    
        DOCUMENTATION: Updated readme and added github token info
    
    commit 717726936b02b2002ede65396a32e1ffadffcc66
    Merge: 7d745ee 1d98f5b
    Author: Gavin Stone <gavsto@gmail.com>
    Date:   Thu Oct 28 16:09:07 2021 +0000
    
        Merge pull request #148 from gavsto/dev
    
        ENHANCEMENT: Added additional cache removal options
    
    commit 1d98f5b6e75c49eab2fe32ae3f5e4aa0b820851c
    Author: Gavsto <gavsto@gmail.com>
    Date:   Thu Oct 28 16:07:17 2021 +0000
    
        ENHANCEMENT: Added additional cache removal options
    
        Added cache removal options for Best Practice Analyser
    
    commit 7d745ee1f5c21bf85044f7a8712e5ecadff50d6a
    Author: Kelvin Tegelaar <kelvin@limenetworks.nl>
    Date:   Thu Oct 28 17:23:49 2021 +0200
    
        timezone adding
    
    commit 6281e6c4637b4df274c00d3b7b6db6e55ace15df
    Author: Kelvin Tegelaar <kelvin@limenetworks.nl>
    Date:   Thu Oct 28 16:01:04 2021 +0200
    
        reads like a recipe book
    
    commit 2dfff1be0d8777bba7cf52aa542586a276423363
    Author: Kelvin Tegelaar <kelvin@limenetworks.nl>
    Date:   Thu Oct 28 15:56:47 2021 +0200
    
        community changes to modal
    
    commit e2cb0f1718223a2578171194a2587eb214c8f5cf
    Author: Kelvin Tegelaar <kelvin@limenetworks.nl>
    Date:   Thu Oct 28 15:54:54 2021 +0200
    
        community decided the modal needs changing
    
    commit 459ae6f761eff1271c445538ec556afe1e40c473
    Author: Kelvin Tegelaar <kelvin@limenetworks.nl>
    Date:   Thu Oct 28 15:48:46 2021 +0200
    
        bpa stuff
    
    commit 36fb47ee2f710aeb696d6509fc0303b51f01045c
    Author: Kelvin Tegelaar <kelvin@limenetworks.nl>
    Date:   Thu Oct 28 15:47:43 2021 +0200
    
        added BPA analysis confirmation and warning
    
    commit dd7402f426a46ba030aa8cf77e77ab620b2e6d43
    Author: Kelvin Tegelaar <kelvin@limenetworks.nl>
    Date:   Thu Oct 28 10:10:26 2021 +0200
    
        removed tenantlist from bpa, does not belong there.

--- js/datatables/datatablesBestPracticeAnalyser.js
+++ js/datatables/datatablesBestPracticeAnalyser.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     var todayDate = new Date().toISOString().slice(0, 10);
     $('.datatable-1').dataTable(
         {
+            "scrollX": true,
             language: {
                 paginate: {
                     next: '<i class="fas fa-arrow-right"></i>',
@@ -31,12 +32,14 @@ $(document).ready(function () {
                 { extend: 'copyHtml5', className: 'btn btn-primary btn-sm' },
                 { extend: 'excelHtml5', className: 'btn btn-primary btn-sm', title: 'Best Practice Analyser - ' + todayDate, exportOptions: { orthogonal: "export" } },
                 { extend: 'csvHtml5', className: 'btn btn-primary btn-sm', title: 'Best Practice Analyser - ' + todayDate, exportOptions: { orthogonal: "export" } },
-                { extend: 'pdfHtml5', className: 'btn btn-primary btn-sm', pageSize: 'A2', orientation: 'landscape', title: 'Best Practice Analyser - ' + todayDate, exportOptions: { columns: [0, 1, 2, 4, 5, 6, 7, 8, 9, 10], orthogonal: "export" } },
+                { extend: 'pdfHtml5', className: 'btn btn-primary btn-sm', pageSize: 'A2', orientation: 'landscape', title: 'Best Practice Analyser - ' + todayDate, exportOptions: { columns: [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11], orthogonal: "export" } },
                 {
                     text: 'Force Refresh All Data',
                     className: 'btn btn-primary btn-sm',
                     action: function (e, dt, button, config) {
-                        window.location = '/api/BestPracticeAnalyser_OrchestrationStarter';
+                        $('#APIContent').html('<center><label class="form-check-label" >Are you sure you want to force the Best Practice Analysis to run? This will slow down normal usage considerably. Please note: this runs at midnight automatically every day. <br /><br /></label><br><nothing class="APIConfirmed"><a href="/api/BestPracticeAnalyser_OrchestrationStarter"><button id="Confirmed" class="btn btn-primary APIConfirmed">Yes</button></a></nothing><nothing class="APIDenied">  <button data-bs-dismiss="modal" class="btn btn-primary APIDenied">No</button></center>');
+
+                        document.getElementById("PopModal").click();
                     }
                 }
             ],
@@ -51,7 +54,11 @@ $(document).ready(function () {
                         }
                         if (data === true) {
                             return '<i class="fas fa-check-circle text-success fa-2x"></i>';
-                        } else {
+                        }
+                        if (data === "") {
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
+                        }
+                        else {
                             return '<i class="fas fa-times-circle text-danger fa-2x"></i></a>';
                         }
                     }
@@ -64,6 +71,9 @@ $(document).ready(function () {
                         }
                         if (data === true) {
                             return '<i class="fas fa-check-circle text-success fa-2x"></i>';
+                        }
+                        if (data === "") {
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
                         } else {
                             return '<i class="fas fa-exclamation-triangle text-warning fa-2x"></i></a>';
                         }
@@ -82,7 +92,7 @@ $(document).ready(function () {
                             return '<button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#' + row.GUID + 'SendAs">' + row.MessageCopyForSendAsCount + ' Users Off</button><!-- Modal --><div class="modal fade" id="' + row.GUID + 'SendAs" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Mailboxes where Message Copy to Send As Not On</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body"> ' + row.MessageCopyForSendList + '</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button></div></div></div></div>'
                         }
                         else {
-                            return '<h5><span class="badge bg-danger">No Information Returned</span></h5>'
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
                         }
                     }
                 },
@@ -94,6 +104,9 @@ $(document).ready(function () {
                         }
                         if (data === true) {
                             return '<i class="fas fa-times-circle text-danger fa-2x"></i>';
+                        }
+                        if (data === "") {
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
                         } else {
                             return '<i class="fas fa-check-circle text-success fa-2x"></i></a>';
                         }
@@ -107,6 +120,9 @@ $(document).ready(function () {
                         }
                         if (data === true) {
                             return '<i class="fas fa-check-circle text-success fa-2x"></i>';
+                        }
+                        if (data === "") {
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
                         } else {
                             return '<i class="fas fa-times-circle text-danger fa-2x"></i></a>';
                         }
@@ -120,6 +136,9 @@ $(document).ready(function () {
                         }
                         if (data === true) {
                             return '<i class="fas fa-exclamation-triangle text-warning fa-2x"></i>';
+                        }
+                        if (data === "") {
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
                         } else {
                             return '<i class="fas fa-check-circle text-success fa-2x"></i></a>';
                         }
@@ -141,7 +160,7 @@ $(document).ready(function () {
                             return '<h5><span class="badge bg-secondary">On Specific Users</span></h5>';
                         }
                         else {
-                            return '<h5><span class="badge bg-danger">No Information Returned</span></h5>'
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
                         }
                     }
                 },
@@ -153,6 +172,9 @@ $(document).ready(function () {
                         }
                         if (data === true) {
                             return '<i class="fas fa-check-circle text-success fa-2x"></i>';
+                        }
+                        if (data === "") {
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
                         } else {
                             return '<i class="fas fa-times-circle text-danger fa-2x"></i></a>';
                         }
@@ -166,21 +188,38 @@ $(document).ready(function () {
                         }
                         if (data > 0) {
                             //return '<h5><span class="badge bg-danger">' + data + ' Users Enabled</span></h5>'
-                            return '<button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#' + row.GUID + 'SharedMailbox">' + row.DisabledSharedMailboxLoginsCount + ' Users Enabled</button><!-- Modal --><div class="modal fade" id="' + row.GUID + 'SharedMailbox" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Shared Mailboxes with Enabled User Accounts</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body"> ' + row.DisabledSharedMailboxLogins + '</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button></div></div></div></div>'
+                            return '<button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#' + row.GUID + 'SharedMailbox">' + row.DisabledSharedMailboxLoginsCount + ' Users Enabled</button><!-- Modal --><div class="modal fade" id="' + row.GUID + 'SharedMailbox" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">Shared Mailboxes with Enabled User Accounts</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body"> ' + row.DisabledSharedMailboxLogins + '</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button></div></div></div></div>'
                         }
                         if (data === 0) {
                             return '<h5><span class="badge bg-success">None Enabled</span></h5>';
                         }
                         else {
-                            return '<h5><span class="badge bg-danger">No Information Returned</span></h5>'
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
+                        }
+                    }
+                },
+                {
+                    "data": "UnusedLicensesResult",
+                    "render": function (data, type, row) {
+                        if (type === "export" || type === "sort" || type === "filter") {
+                            return data;
+                        }
+                        if (data === "FAIL") {
+                            return '<button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#' + row.GUID + 'UnusedLicenses">' + row.UnusedLicensesCount + ' SKUs Unassigned Licenses</button><!-- Modal --><div class="modal fade" id="' + row.GUID + 'UnusedLicenses" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLabel">SKUs with Unassigned Licenses</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body"> ' + row.UnusedLicenseList + '</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button></div></div></div></div>'
+                        }
+                        if (data === "PASS") {
+                            return '<h5><span class="badge bg-success">No Unused Licenses</span></h5>';
+                        }
+                        else {
+                            return '<h5><span class="badge bg-secondary">No Data</span></h5>'
                         }
                     }
                 }
             ],
             'columnDefs': [
                 {
-                    "targets": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], // your case first column
-                    "className": "text-center"
+                    "targets": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], // your case first column
+                    "className": "text-center align-middle"
                 }
             ],
             "order": [[0, "asc"]],
